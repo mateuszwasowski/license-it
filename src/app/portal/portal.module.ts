ec43917988c5fac0e36fd83928d78361dc50c18b
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2017-05-07 10:01:29 +0200

    uczelnia stuff

--- src/app/portal/portal.module.ts
+++ src/app/portal/portal.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import {FormsModule} from '@angular/forms';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
-  declarations: [IndexComponent, NotFoundComponent],
-  exports: [IndexComponent, NotFoundComponent]
+  declarations: [IndexComponent, NotFoundComponent, LoginComponent],
+  exports: [IndexComponent, NotFoundComponent, LoginComponent]
 })
 export class PortalModule { }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { IndexComponent } from './portal/index/index.component';
+import { LoginComponent } from './portal/login/login.component';
 import { NotFoundComponent} from './portal/not-found/not-found.component';
 
 export const appRoutes: Routes = [
     {path: '', component: IndexComponent},
+    {path: 'login', component: LoginComponent},
     {path: '**', component: NotFoundComponent}
 ];
